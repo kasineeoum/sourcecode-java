4cecd271c0dd0eb4d8ad8e7385b4f065dedbf1e2
kasineeoum <oumoem2537@gmail.com>
2018-07-26 11:07:10 +0700

    First commit

---
+++ whatever/src/FizzBuzz.java
@@ -0,0 +1,13 @@
+
+public class FizzBuzz {
+
+    public String sendAndReturn(int nuber) {
+        if (nuber ==2) {
+        return "2";
+    }
+        return "1";
+        
+
+    }
+
+}
---
+++ whatever/src/WorkshopStringEqal.java
@@ -0,0 +1,9 @@
+
+public class WorkshopStringEqal {
+
+    public static void main(String[] args) {
+        String input ="";
+
+    }
+
+}
